c1e682ae04d2a6477c69ccbbc08e315e282c247a
Diego Cáceres <mavroslykanthropos@outlook.com>
2026-03-25 22:00:18 -0300

    Implementar pantalla de inicio SPPB y componente Side-by-Side
    Reemplaza la plantilla por defecto por una interfaz diseñada para las pruebas SPPB. Incluye un cronómetro funcional para la prueba de equilibrio "Side-by-Side" utilizando Angular Signals, con un límite automático de 10 segundos.

---
+++ src/app/home/semi-tandem/semi-tandem.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-semi-tandem',
+  templateUrl: './semi-tandem.component.html',
+  styleUrls: ['./semi-tandem.component.scss'],
+})
+export class SemiTandemComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
